dabf73cb195686ab759ae45fadc98f9c0238743f
Andis Sprinkis <mail@andis.work>
2021-08-26 22:04:33 +0300

    config

--- neoray-config.cmd
+++ neoray-config.cmd
@@ -1,4 +1,4 @@
 set programExeFilename=neoray-win64.exe
 set programExePath=D:\\win\\portable-app\\neoray\\%programExeFilename%
 set iconPath=%programExePath%
-set programName=Neoray
+set programName=neoray
