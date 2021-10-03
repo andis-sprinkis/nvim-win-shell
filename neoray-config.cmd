225f21eccead86abf309c95e5a60e8a22d83bb5a
Andis Sprinkis <mail@andis.work>
2021-10-03 18:41:59 +0300

    capitalize name

--- neoray-config.cmd
+++ neoray-config.cmd
@@ -1,5 +1,5 @@
 set programExeFilename=neoray-win64.exe
 set programExePath=D:\\win\\portable-app\\neoray\\%programExeFilename%
 set iconPath=%programExePath%
-set programName=neoray
+set programName=Neoray
 set applyFileTypeAssociations=1
