abd10e1a1a58b39e345bf556251b6f8397e5bd77
Andis Sprinkis <mail@andis.work>
2021-10-08 08:05:23 +0300

    Make adding and removal of context menu options optional

--- common/add.cmd
+++ common/add.cmd
@@ -3,86 +3,56 @@ setlocal enabledelayedexpansion
 cd /d %~dp0
 
 if not exist %~dp0..\%1 (
-  echo Specified config file "%1" is not found.
+  echo Specified config file "%1" is not found. Exiting..
   goto :Error
-)
-
-call %~dp0..\%1
-set setUserFTAZip=%~dp0SetUserFTA.zip
-set setUserFTADir=%~dp0SetUserFTA
-set setUserFTAExe=%setUserFTADir%\SetUserFTA\SetUserFTA.exe
-set setUserFTAURL=https://kolbi.cz/SetUserFTA.zip
-set downloadCMD=bitsadmin /transfer myDownloadJob /download /priority normal
-set programFileAssociationsFile=%~dp0..\filetypes.txt
-set contextMenuIconString=Edit with %programName%
-set contextMenuBackgroundString=Open %programName% here
-set programProgId=Applications\%programExeFilename%
-
-echo[
-echo Setting up SetUserFTA utility to add %programExeFilename% ProgID file type associations...
-:SetUserFTASetup
-if exist %setUserFTAExe% (
-  goto :MainSetup
-) else if exist %setUserFTAZip% (
-  echo[
-  echo - Unzipping SetUserFTA utility...
-  call :UnZipFile "%setUserFTADir%" "%setUserFTAZip%"
-  rm %setUserFTAZip%
-  goto :MainSetup
 ) else (
-  echo[
-  echo - Downloading SetUserFTA utility...
-  %downloadCMD% %setUserFTAURL% %setUserFTAZip%
-  goto :SetUserFTASetup
-)
+  call %~dp0..\%1
 )
 
-:MainSetup
+call %~dp0variables.cmd
 
 echo[
-echo Setting %programProgId% ProgID...
+echo Adding %programProgId% ProgID...
 reg add "HKCR\Applications\%programExeFilename%\shell\open\command" /t REG_SZ /d "%programExePath% ""%%1""" /f
+echo Adding %programProgId% ProgID has completed.
 
-echo[
-echo Setting Explorer icon ^& background '%contextMenuIconString%' right click context menu option...
-reg add "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%" /v Icon /t REG_SZ /d "%iconPath%" /f
-reg add "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%\command" /t REG_SZ /d "%programExePath% ""%%1""" /f
-reg add "HKCR\Drive\shell\%contextMenuIconString%" /v Icon /t REG_SZ /d "%iconPath%" /f
-reg add "HKCR\Drive\shell\%contextMenuIconString%\command" /t REG_SZ /d "%programExePath% ""%%1""" /f
-reg add "HKCR\Directory\Background\shell\%contextMenuBackgroundString%" /v Icon /t REG_SZ /d "%iconPath%" /f
-reg add "HKCR\Directory\Background\shell\%contextMenuBackgroundString%\command" /t REG_SZ /d "%programExePath% ""%%V""" /f
-
-if "%applyFileTypeAssociations%"=="1" (
+if "%setContextMenu%"=="1" (
   echo[
-  echo Setting the file type associations...
+  echo Adding right click context menu options...
+
+  if "%setContextMenuHKCRAllFileSystemObjects%"=="1" (
+    reg add "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%" /v Icon /t REG_SZ /d "%iconPath%" /f
+    reg add "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%\command" /t REG_SZ /d "%programExePath% ""%%1""" /f
+  )
+
+  if "%setContextMenuHKCRDrive%"=="1" (
+    reg add "HKCR\Drive\shell\%contextMenuIconString%" /v Icon /t REG_SZ /d "%iconPath%" /f
+    reg add "HKCR\Drive\shell\%contextMenuIconString%\command" /t REG_SZ /d "%programExePath% ""%%1""" /f
+  )
+
+  if "%setContextMenuHKCRBackground%"=="1" (
+    reg add "HKCR\Directory\Background\shell\%contextMenuBackgroundString%" /v Icon /t REG_SZ /d "%iconPath%" /f
+    reg add "HKCR\Directory\Background\shell\%contextMenuBackgroundString%\command" /t REG_SZ /d "%programExePath% ""%%V""" /f
+  )
+
+  echo Adding right click context menu options has completed.
+)
+
+if "%setFileTypeAssociations%"=="1" (
+  call %~dp0SetUserFTASetup.cmd
+
+  echo[
+  echo Setting %programProgId% file type associations...
   for /F "tokens=*" %%A in (%programFileAssociationsFile%) do %setUserFTAExe% .%%A %programProgId%
+  echo Setting %programProgId% file type associations has completed.
 )
 
 echo[
-echo FINISHED
+echo Script finished.
 endlocal
 exit /B %errorlevel%
-
-:: VBS to exract ZIP files
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\unzip.vbs"
->>%vbs% echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
 
 :Error
+
 endlocal
 exit /B 1
-
-echo[
-echo FINISHED
-endlocal
-exit /B %errorlevel%
--- common/remove.cmd
+++ common/remove.cmd
@@ -1,34 +1,41 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call %~dp0..\%1
-
 if not exist %~dp0..\%1 (
-  echo Specified config file "%1" is not found.
+  echo Specified config file "%1" is not found. Exiting...
   goto :Error
+) else (
+  call %~dp0..\%1
+)
+call %~dp0variables.cmd
+
+call %~dp0SetUserFTASetup.cmd
+
+echo[
+echo Removing %programProgId% file type associations...
+for /F "tokens=*" %%A in (%programFileAssociationsFile%) do %setUserFTAExe% del .%%A
+echo %programProgId% file type associations removal has completed.
+
+if "%removeOnlyFileTypeAssociations%"=="0" (
+  echo[
+  echo Removing %programProgId% ProgID...
+  reg delete "HKCR\Applications\%programExeFilename%" /f
+  echo %programProgId% ProgID removal has completed.
+
+  echo[
+  echo Removing right click context menu items...
+  reg delete "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%" /f
+  reg delete "HKCR\Drive\shell\%contextMenuIconString%" /f
+  reg delete "HKCR\Directory\Background\shell\%contextMenuBackgroundString%" /f
+  echo Right click context menu items removal has completed.
 )
 
-set contextMenuIconString=Edit with %programName%
-set contextMenuBackgroundString=Open %programName% here
-set programProgId=Applications\%programExeFilename%
-set programFileAssociationsFile=%~dp0..\filetypes.txt
-
 echo[
-echo Removing %programExeFilename% ProgID and coresponding file type associations...
-for /F "tokens=*" %%A in (%programFileAssociationsFile%) do %setUserFTAExe% del .%%A
-reg delete "HKCR\Applications\%programExeFilename%" /f
-
-echo[
-echo Removing Explorer icon ^& background '%contextMenuIconString%' right click context menu items...
-reg delete "HKCR\AllFilesystemObjects\shell\%contextMenuIconString%" /f
-reg delete "HKCR\Drive\shell\%contextMenuIconString%" /f
-reg delete "HKCR\Directory\Background\shell\%contextMenuBackgroundString%" /f
-
-:Error
-endlocal
-exit /B 1
-
-echo[
-echo FINISHED
+echo Script finished.
 endlocal
 exit /B %errorlevel%
+
+:Error
+
+endlocal
+exit /B 1
